2f55bcded1bee2d101e28073f3ba6d70e00f480d
SANNIDHI RAJENDRAKUMAR ANVEKAR <anvekarsannidhi@gmail.com>
2026-05-31 17:02:52 +0530

    Prepared TaskFlow of deployment

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f7u7gte0=_&+7x!^56!t0b3-!16j_=u8tkbcj-6*a#xh!pb2#f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
